a19fae743715566c6b11b067971a5637c97444d5
Yadira Fleitas <yadyguitar@yahoo.es>
2017-02-23 15:40:24 -0600

    Casi listo la parte de datos en fondo del pozo, agregue JFreeChart para las graficas y falta embeberlas...

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,22 @@
 package application;
 	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws IOException  {
+		Parent mainLayout = FXMLLoader.load(getClass().getResource("interfazFXML.fxml"));
+        Scene scene = new Scene(mainLayout, 1100, 600);
+        primaryStage.setTitle("Sistema Nuvoil");
+        primaryStage.setScene(scene);
+		primaryStage.setMaximized(true);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
